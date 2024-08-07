760c365020d1e235695a2b82d76a3f3d34026396
Emirhan <emirhan_gungor2009@hotmail.com>
2024-08-07 10:26:48 +0300

    ProductFactory Fixed

--- App/Models/ProductFactory.php
+++ App/Models/ProductFactory.php
@@ -6,31 +6,20 @@ use App\Models\Types\Book;
 use App\Models\Types\DVD;
 use App\Models\Types\Furniture;
 
-class ProductFactory
-{
-    public function createProduct($productType): Product
-    {
-        switch ($productType) {
-            case 'Book':
-                return new Book();
-            case 'DVD':
-                return new DVD();
-            case 'Furniture':
-                return new Furniture();
-            default:
-                // Handle invalid product type
-                throw new \InvalidArgumentException("Invalid product type: $productType");
+class ProductFactory {
+    private static $productClasses = [
+        'Book' => Book::class,
+        'DVD' => DVD::class,
+        'Furniture' => Furniture::class
+    ];
+
+    public static function createProduct($type, $params = []) {
+        if (!isset(self::$productClasses[$type])) {
+            throw new Exception("Invalid product type: " . $type);
         }
+        $className = self::$productClasses[$type];
+        return new $className(...$params);
     }
 }
-
-
-
-
-
-
-
-
-
 
 ?>
